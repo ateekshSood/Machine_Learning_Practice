77e62b23e40cd1e026601cc7618cd7095dac0263
Ateeksh <ateesood@gmail.com>
2026-03-04 07:33:18 +0000

    random test file

--- TES/test.py
+++ TES/test.py
@@ -1,17 +1,6 @@
-import numpy as np
+dict = {'one' : 2,
+        'two' : 1}
 
-# BOOLEAN STUFF 
-
-# Imagine you have a list of numbers representing the "Categories" of items in a small dataset:
-# categories = [0, 1, 2, 0, 3, 2, 1, 0]
-
-# Question 1: The "Bouncer" Test
-# Goal: Create a list of True and False (a mask) that identifies only the zeros.
-
-categories = np.array([0, 1, 2, 0, 3, 2, 1, 0])
-mask = categories == 0
-print(mask)
-
-# Question 2: The "Either/Or" (The | Operator)
-# Goal: You want to find both category 0 and category 2.
-
+yeah = sorted(dict.items() ,  key=lambda x : x[1] , reverse=True)
+keys_sort = [key[0] for key in yeah]
+print(keys_sort)
